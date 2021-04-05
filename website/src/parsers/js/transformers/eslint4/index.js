8329f7fb4c96ca2aa1dd0117057d16e7268655d0
Rob Stolarz <rob@lattice.com>
2021-04-05 14:35:41 -0700

    WIP: destructively replace ESLint v4 w/ v7

--- website/src/parsers/js/transformers/eslint4/index.js
+++ website/src/parsers/js/transformers/eslint4/index.js
@@ -12,18 +12,14 @@ export default {
   defaultParserID: 'babel-eslint',
 
   loadTransformer(callback) {
-    require(
-      [
-        'eslint4/lib/linter',
-        'eslint4/lib/util/source-code',
-        '../../utils/eslint4Utils',
-      ],
-      (Linter, sourceCode, utils) => callback({eslint: new Linter(), sourceCode, utils}),
-    );
+    require(['eslint4/lib/linter', '../../utils/eslint4Utils'], (
+      { Linter },
+      utils,
+    ) => callback({ eslint: new Linter(), utils }));
   },
 
-  transform({ eslint, sourceCode, utils }, transformCode, code) {
+  transform({ eslint, utils }, transformCode, code) {
     utils.defineRule(eslint, transformCode);
-    return utils.runRule(code, eslint, sourceCode);
+    return utils.runRule(code, eslint);
   },
 };
--- website/src/parsers/js/utils/eslint4Utils.js
+++ website/src/parsers/js/utils/eslint4Utils.js
@@ -1,6 +1,9 @@
 import compileModule from '../../utils/compileModule';
 import transpile from '../../transpilers/babel';
-import {parseNoPatch} from 'babel-eslint';
+import { parseNoPatch } from 'babel-eslint';
+import { parse as tsEsParse } from '@typescript-eslint/parser';
+
+const esquery = require('esquery');
 
 export function formatResults(results) {
   return results.length === 0
@@ -9,7 +12,8 @@ export function formatResults(results) {
 }
 
 export function formatResult(result) {
-  var pointer = '-'.repeat(result.column - 1) + '^';
+  console.log(result);
+  var pointer = '-'.repeat((result.column || 1) - 1) + '^';
   return `
 // ${result.message} (at ${result.line}:${result.column})
    ${result.source}
@@ -18,38 +22,66 @@ export function formatResult(result) {
 }
 
 export function defineRule(eslint, code) {
+  console.log('things are still compiling');
   // Compile the transform code and install it as an ESLint rule. The rule
   // name doesn't really matter here, so we'll just use a hard-coded name.
   code = transpile(code);
   const rule = compileModule(code);
-    eslint.defineRule('astExplorerRule', rule.default || rule);
+  eslint.defineRule('astExplorerRule', rule.default || rule);
 }
 
 export function runRule(code, eslint) {
   // Run the ESLint rule on the AST of the provided code.
   // Reference: http://eslint.org/docs/developer-guide/nodejs-api
+  eslint.defineParser('@typescript-eslint/parser', {
+    parse(code) {
+      try {
+        console.log(esquery);
+        // why all the options? because eslint adds them and this expects them
+        // https://github.com/typescript-eslint/typescript-eslint/issues/2742#issuecomment-723481686
+        return tsEsParse(code, {
+          sourceType: 'module',
+          loc: true,
+          range: true,
+          raw: true,
+          tokens: true,
+          comment: true,
+          eslintVisitorKeys: true,
+          eslintScopeManager: true,
+          filePath: 'test.tsx',
+        });
+      } catch (e) {
+        console.error(e);
+        throw e;
+      }
+    },
+  });
   eslint.defineParser('babel-eslint', {
     parse(code) {
       return parseNoPatch(code, { sourceType: 'module' });
     },
   });
-  const results = eslint.verifyAndFix(code, {
-    env: {es6: true},
-    parser: 'babel-eslint',
-    parserOptions: {
-      ecmaVersion: 8,
-      sourceType: 'module',
-      ecmaFeatures: {experimentalObjectRestSpread: true},
+  const results = eslint.verifyAndFix(
+    code,
+    {
+      env: { es6: true },
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: { experimentalObjectRestSpread: true, jsx: true },
+      },
+      rules: {
+        astExplorerRule: 2,
+      },
     },
-    rules: {
-      astExplorerRule: 2,
-    },
-  });
+    { filename: 'test.tsx' },
+  );
   let output = formatResults(results.messages);
   output += `
 
 // Fixed output follows:
-// ${ '-'.repeat(80) }
+// ${'-'.repeat(80)}
 `;
   return output + results.output;
 }
